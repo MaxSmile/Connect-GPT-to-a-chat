fc56d038e607e1ad28cf8dbe94c9b6438e97d450
Maxim Vasilkov <dev@smrt16.com>
2023-11-25 15:01:10 +0200

    demo

--- ai-chat-tool/pages/demo.js
+++ ai-chat-tool/pages/demo.js
@@ -1,7 +1,23 @@
- export default function Demo () {
-    return (
-        <div>
-            <h1>Demo</h1>
-        </div>
-    )
+// pages/demo.js
+import { useEffect, useState } from 'react';
+
+export default function Demo() {
+  const [logs, setLogs] = useState('');
+
+  useEffect(() => {
+    async function fetchLogs() {
+      const response = await fetch('/api/logs');
+      const data = await response.json();
+      setLogs(data.logs);
+    }
+
+    fetchLogs();
+  }, []);
+
+  return (
+    <div>
+      <h1>Project Logs</h1>
+      <pre>{logs}</pre>
+    </div>
+  );
 }
