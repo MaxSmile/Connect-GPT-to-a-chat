9ab142b4360f504cc51d1986ab9b55ff86df138d
sarlove <>
2023-11-25 15:52:24 +0100

    changed colors on the button

--- ai-chat-tool/pages/index.js
+++ ai-chat-tool/pages/index.js
@@ -10,17 +10,16 @@ export default function Home() {
     >
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <a className="flex items-center gap-2" href="/demo">
-          <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-            Get started with DEMO
-          </p>
-          </a>
+        <p className="w-full max-w-md mx-auto text-center py-3 rounded-lg text-white bg-gradient-to-br from-blue-600 to-teal-500 hover:from-blue-600 hover:to-teal-400 shadow-lg px-4">
+          Get started with DEMO
+        </p>
+        </a>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             href="https://vasilkoff.com"
             target="_blank"
-            rel="noopener noreferrer"
-          >
+            rel="noopener noreferrer">
             Powered By Vasilkoff Team {' '}
 
           </a>
@@ -86,8 +85,7 @@ export default function Home() {
           href="#"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer"  >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Time to Celebrate!
           </h2>
