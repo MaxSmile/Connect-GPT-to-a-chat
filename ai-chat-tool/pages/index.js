287ff5c33c57027ed69a19855dad7ae234656126
sarlove <>
2023-11-25 14:03:34 +0100

    remove links

--- ai-chat-tool/pages/index.js
+++ ai-chat-tool/pages/index.js
@@ -40,7 +40,7 @@ export default function Home() {
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
       <a
-          href="#"
+          
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
@@ -55,7 +55,6 @@ export default function Home() {
       </a>
 
         <a
-          href="#"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
@@ -69,7 +68,7 @@ export default function Home() {
         </a>
 
         <a
-          href="#"
+          
            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
@@ -83,7 +82,7 @@ export default function Home() {
         </a>
 
         <a
-          href="#"
+          
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
